a43b2511547bd87a335d9c86aafde7e8976f3f8a
mitchmcnaughton <141999182+mitchmcnaughton@users.noreply.github.com>
2023-11-19 17:23:30 +1100

    added the category,product,producttag and tag models and linked them in index.js

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const Category = require('./Category');
+const Product = require('./Product');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category,{
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+  });
+
+Category.hasMany(Product, {
+    foreignKey: 'reader_id',
+    onDelete: 'CASCADE',
+  });
+
+Product.belongsToMany(Tag, {
+    through: ProductTag
+});
+
+Tag.belongsToMany(Product, {
+     through: ProductTag
+});
